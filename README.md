b8bd636b2a410d879048d8809c7468a1b1721c32
Naman Nehra <naman.nehra98@gmail.com>
2016-03-02 23:16:00 +0530

    First release

---
+++ README.md
@@ -0,0 +1,41 @@
+# dragJS
+JavaScript functions for listening to `drag` events.
+
+[Example](https://namannehra.github.io/dragJS/)
+
+## Setup
+Load the file using script tag in head
+
+	<script src="drag-transformed.js"></script>
+
+## Use
+Call `getDragEvent` and pass an element as argumenmt
+
+	var d = document.querySelector('#d');
+	getDragEvent(d);
+
+Now this element will fire drag events. Listen using addEventListener. The `detail` of event has following properties:
+Property | Type | Description
+- | - | -
+state | String | 'start', 'drag' or 'end'
+startX | Number | x coordinate where drag started in pixel
+startY | Number | y coordinate where drag started in pixel
+dx | Number | x coordinate of drag in pixel
+dy | Number | y coordinate of drag in pixel
+sourceEvent | Object | Original Pointer Event which is used it calculate drag
+
+Call `stopDragEvent` when you don't need the element to fire drag event as this event
+
+	stopOverscrollEvent(d);
+
+`firesDragEvent` can be used to check if an element is firing overscroll events.
+
+	firesOverscrollEvent(d); //true
+
+`drag.js` is the original file and uses `const`, `let` and `arrow functions` which may not be supported in all browsers. `drag-transformed.js` is genetared using [babeljs](https://babeljs.io/) and will work in most browsers.
+
+## Notes
+* Pointer events are required for drag events to work. Use [PEP](https://github.com/jquery/PEP) to polyfill pointer events.
+* Set `touch-action` CSS property (`touch-action` attribute if using PEP) so drag events work with touch.
+* Set `user-select` CSS property to none to prevent selection while draging.
+* `sourceEvent` property can be use to check `isPrimary`, `pointerId`, `clientX`, `clientY` or to call `preventDefault`.
---
+++ drag.js
@@ -0,0 +1,76 @@
+(() => {
+	'use strict';
+	if ('getDragEvent' in window) return;
+	const des = 'dragEventProperties',
+				sym = 'Symbol' in window ? Symbol(des) : `_${des}`,
+				pointers = {};
+	let hasEvents = false,
+			activePointers = 0;
+	const check = (e, f) => {
+		if (!e || e.nodeType !== 1) throw new TypeError(`Failed to execute '${f}' with '${e}': parameter 1 should be a node with nodeType equal to 1`);
+	};
+	const dispatch = (e, pointer, state) => {
+		pointer.target.dispatchEvent(new CustomEvent('drag', {
+			detail: {
+				state,
+				startX: pointer.startX,
+				startY: pointer.startY,
+				dx: e.clientX - pointer.startX,
+				dy: e.clientY - pointer.startY,
+				sourceEvent: e
+			}
+		}));
+	};
+	const pointerdown = function(e) {
+		pointers[e.pointerId] = {
+			start: true,
+			startX: e.clientX,
+			startY: e.clientY,
+			target: this
+		};
+		activePointers++;
+		if (!hasEvents && activePointers > 0) {
+			hasEvents = true;
+			document.addEventListener('pointermove', pointermove);
+			document.addEventListener('pointerup', pointerup);
+		}
+	};
+	const pointermove = e => {
+		const pointer = pointers[e.pointerId];
+		if (pointer === undefined) return;
+		if (pointer.target[sym] === undefined) {
+			delete pointers[e.pointerId];
+			return;
+		}
+		const start = pointer.start;
+		if (start) pointer.start = false;
+		dispatch(e, pointer, start ? 'start' : 'drag');
+	};
+	const pointerup = e => {
+		const pointer = pointers[e.pointerId];
+		if (pointer === undefined) return;
+		delete pointers[e.pointerId];
+		activePointers--;
+		if (hasEvents && activePointers === 0) {
+			hasEvents = false;
+			document.removeEventListener('pointermove', pointermove);
+			document.removeEventListener('pointerup', pointerup);
+		}
+		if (!pointer.start) dispatch(e, pointer, 'end');
+	};
+	window.getDragEvent = e => {
+		check(e, 'getDragEvent');
+		if (sym in e) return;
+		e[sym] = true;
+		e.addEventListener('pointerdown', pointerdown);
+	};
+	window.stopDragEvent = e => {
+		check(e, 'stopDragEvent');
+		delete e[sym];
+		e.removeEventListener('pointerdown', pointerdown);
+	};
+	window.firesDragEvent = e => {
+		check(e, 'firesDragEvent');
+		return sym in e;
+	};
+})();
